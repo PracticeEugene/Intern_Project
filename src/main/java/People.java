464dde949f6c6b10b13e8f07abd52998574c0b8f
YevhenBobukh <eugene.bobukh@gmail.com>
2023-08-17 16:46:11 +0300

    First commit

--- src/main/java/People.java
+++ src/main/java/People.java
@@ -2,7 +2,7 @@ public abstract class People {
     private final String sex;
     private final String name;
     private final int age;
-    private final JobType job;
+    private final JobType jobb;
 
     public People(String sex, String name, int age, JobType job) {
         this.sex = sex;
